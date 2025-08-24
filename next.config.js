22c9d9a2bfd8d18c6993b879163eaf4a67fe0995
JeanR1802 <rs.cedora@gmail.com>
2025-08-24 02:43:32 -0700

    Initial commit - actualizacion del slug

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-}
+  // --- Configuración para subdominios ---
+  assetPrefix: isProd ? "https://gestularia.com" : "",
+  // Opcional: para que Next.js genere correctamente rutas relativas en build
+  basePath: "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
